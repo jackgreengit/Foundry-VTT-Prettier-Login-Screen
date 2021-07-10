f0fa018dec9d8d1561f622d8b9524e9124241207
jackgreengit <60493009+jackgreengit@users.noreply.github.com>
2021-07-10 01:59:07 +0100

    Update patch_url.sh

--- patch_url.sh
+++ patch_url.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/jackgreengit/Foundry-VTT-Prettier-Login-Screen/main/foundry_login.css -O patchStyle.css
+wget https://raw.githubusercontent.com/jackgreengit/Foundry-VTT-Prettier-Login-Screen/main/newTest.css -O patchStyle.css
 cat patchStyle.css >> /home/foundry/resources/app/public/css/style.css
 rm patchStyle.css
